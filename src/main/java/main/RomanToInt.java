3a2301b661b6d1c7980ac235bbecaac7d0568865
joelKamps <joel.kamps1@gmail.com>
2017-10-24 21:16:28 +0200

    make split functionallity green

--- src/main/java/main/RomanToInt.java
+++ src/main/java/main/RomanToInt.java
@@ -12,6 +12,12 @@ public class RomanToInt {
     public List<String> splitString(String givenString) {
         List<String> stringList = new ArrayList<String>();
 
+        String [] parts = givenString.split("");
+
+        for (String part : parts){
+            stringList.add(part);
+        }
+        System.out.println(stringList.toString());
         return stringList;
     }
 }
